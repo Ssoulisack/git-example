b4d25a3401feac7e5454baad70407b453f882540
Ssoulisack <selamoon21@gmail.com>
2022-10-16 16:37:07 +0700

    javascript function 4 ways

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log("hello git")
-console.log('Employe')
+//javascript function 4 ways
+
+//  function declaration
+function square(x) {
+    return x * x;
+}
+//function expression
+const square =  function(x) {
+    return x * x;
+}
+//arrow function expression
+const square = (x) => {
+    return x * x;
+}
+//concise Arrow Function Expression
+const square = x => x * x;
